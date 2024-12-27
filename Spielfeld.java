97b4df150ba2ed8ebd6b1b15250ddf12157a4faf
Caius-Ange <caius-ange.tawotchuen@mni.thm.de>
2024-12-27 20:58:34 +0100

    need to be check

--- Spielfeld.java
+++ Spielfeld.java
@@ -118,7 +118,7 @@ public class Spielfeld {
         spieler2.setAnzahlStein(12);
 
         int[] feld = new int[64];
-        for (int i = 0; i < 64; i++) {
+        for (int i = 0; i < 64; i++) { 
             if (i / 8 < 3 && (i + i / 8) % 2 == 1) {
                 feld[i] = Stein.WEISS_STEIN.getWert();
             } else if (i / 8 > 4 && (i + i / 8) % 2 == 1) {
@@ -143,13 +143,21 @@ public class Spielfeld {
             spielEndeBehandeln();
             return false;
         }
-
+        // Vérifiez et limitez les appels à la logique du joueur
+        boolean zugErgebnis;
         if (getAktuellerSpieler().isKI()) {
-            return kiZugBehandeln();
+            zugErgebnis = kiZugBehandeln();
         } else {
-            return spielerZugBehandeln(startIndex, zielIndex);
+            zugErgebnis =  spielerZugBehandeln(startIndex, zielIndex);
         }
+
+        if (zugErgebnis) {
+            wechselSpieler(); // Change le joueur seulement après un coup valide
+        }
+    
+        return zugErgebnis;
     }
+    
 
     private void spielEndeBehandeln() {
         System.out.println("Das Spiel ist bereits beendet. Gewinner: " + getGewinner());
@@ -633,7 +641,7 @@ class BestenPunktestandVerwalten {
 }
 
 class MinimaxSpieler {
-    private static final int MAX_TIEFE = 10;
+    private static final int MAX_TIEFE = 3;
 
     public static int[] findeBestenZug(Spielfeld spielfeld) {
         int besterWert = Integer.MIN_VALUE;
@@ -844,3 +852,180 @@ class MinimaxSpieler {
         return false;
     }
 }
+
+// Classe pour l'affichage graphique du plateau
+class Print {
+    // Déclaration des variables
+    Turtle turtle;
+    int tailleCase = 60; // Taille d'une case
+    int[] board;
+    Spielfeld spielfeld;
+
+    public Print(Spielfeld spielfeld) {
+
+        this.spielfeld = spielfeld;
+        // Position initiale pour commencer au coin supérieur gauche
+        this.turtle = initTurtle();
+
+        // Initialisation du tableau
+        this.board = spielfeld.getFeld();
+
+        // Dessiner le damier
+        drawBoard(turtle, tailleCase);
+
+        // Afficher le damier
+        printBoard(this.board);
+    }
+
+    Turtle initTurtle() {
+        Turtle newTurtle = new Turtle(480, 480);
+        newTurtle.penUp();
+        newTurtle.backward(240);
+        newTurtle.left(90);
+        newTurtle.forward(240);
+        newTurtle.right(90);
+        newTurtle.penDown();
+
+        return newTurtle;
+    }
+
+    
+    
+
+
+    public void drawSquareAndFill(Turtle t, int tailleCase, int r, int g, int b) {
+        t.penDown();
+
+        for (int i = 0; i < 4; i++) {
+            t.forward(tailleCase);
+            t.right(90);
+        }
+
+        for (int i = 0; i < tailleCase; i++) {
+            t.color(r, g, b);
+            t.penUp();
+            t.right(90);
+            t.forward(1);
+            t.left(90);
+            t.penDown();
+            t.forward(tailleCase);
+            t.penUp();
+            t.backward(tailleCase);
+        }
+        t.penUp();
+        t.right(90);
+        t.backward(tailleCase);
+        t.left(90);
+        t.forward(tailleCase);
+    }
+
+    // method to draw a Pawn with a given color
+    public void drawPawn(Turtle t, int indexZuKoordinaten, int tailleCase, int r, int g, int b) {
+
+        t.penUp();
+        int[] coordinates = indexToCoordinates(indexZuKoordinaten);
+        t.moveTo(coordinates[0] * tailleCase, coordinates[1] * tailleCase);
+
+        t.forward(tailleCase / 2); // Aller au centre de la case
+        t.right(90); // Tourner pour se positionner au centre
+        t.forward(tailleCase / 3 - 5); // S'ajuster légèrement pour éviter de dessiner sur les bords
+        t.left(90); // Tourner à nouveau pour dessiner dans la bonne direction
+        t.color(r, g, b); // Couleur du pion
+
+        drawCircle(t, tailleCase / 4, r, g, b);
+
+        // Revenir à la position initiale
+        t.backward(tailleCase / 2 - 5); // Revenir au centre de la case
+        t.left(90); // Tourner pour retourner à la position initiale
+        t.backward(tailleCase / 2); // Revenir au départ
+        t.right(90); // Tourner pour rétablir l'orientation initiale
+    }
+
+    int[] indexToCoordinates(int index) {
+        int[] coordinates = new int[2];
+        coordinates[0] = index % 8;
+        coordinates[1] = index / 8;
+        System.out.println("Index: " + index + " -> Coords: " + coordinates[0] + ", " + coordinates[1]);
+        return coordinates;
+    }
+
+    void drawCircle(Turtle t, int tailleCercle, int r, int g, int b) {
+        t.penDown();
+        for (int i = 0; i < 36; i++) {
+            t.forward(2 * Math.PI * tailleCercle / 36);
+            t.right(10);
+        }
+
+        // draw the circle with a given color
+        for (int i = 0; i < 36; i++) {
+            t.color(r, g, b);
+            t.penUp();
+            t.right(10);
+            t.forward(2 * Math.PI * tailleCercle / 36);
+            t.penDown();
+            t.forward(2 * Math.PI * tailleCercle / 36);
+            t.penUp();
+            t.backward(2 * Math.PI * tailleCercle / 36);
+        }
+        t.penUp();
+    }
+
+    void drawBoard(Turtle t, int tailleCase) {
+        boolean isStartWithLightBrown = true;
+
+        for (int i = 0; i < 64; i++) {
+            if (i % 8 == 0 && i != 0) {
+                isStartWithLightBrown = !isStartWithLightBrown;
+                t.penUp();
+                t.backward(8 * tailleCase);
+                t.right(90);
+                t.forward(tailleCase);
+                t.left(90);
+                t.penDown();
+            }
+            if (isStartWithLightBrown) {
+                drawSquareAndFill(t, tailleCase, 255, 228, 196);
+                isStartWithLightBrown = false;
+            } else {
+                drawSquareAndFill(t, tailleCase, 139, 69, 19);
+                isStartWithLightBrown = true;
+            }
+        }
+    }
+
+
+    void printBoard(int[] boardToPrint) {
+        Turtle newTurtle = initTurtle();
+
+        drawBoard(newTurtle, tailleCase);
+
+        for (int i = 0; i < 64; i++) {
+            switch (boardToPrint[i]) {
+
+                case -1:
+                    drawPawn(newTurtle, i, tailleCase, 0, 0, 255);
+                    break;
+                case 1:
+                    drawPawn(newTurtle, i, tailleCase, 255, 0, 0);
+                    break;
+                case 2:
+                    drawPawn(newTurtle, i, tailleCase, 255, 255, 255);
+                    break;
+                case -2:
+                    drawPawn(newTurtle, i, tailleCase, 255, 255, 255);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+    public void brettAktualisieren() {
+        this.board = spielfeld.getFeld();
+        printBoard(this.board);
+    }
+ }
+
+    
